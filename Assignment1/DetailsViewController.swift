8b3da7117c92130d1f9319e86dfe621ca9fb5189
evan <w0816747@myscc.ca>
2024-09-16 15:39:46 -0400

    -Added snapshot to add movies to the tableView -Added a details page that displays movie information

---
+++ Assignment1/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  Assignment1
+//
+//  Created by Evan Proulx on 2024-09-16.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
